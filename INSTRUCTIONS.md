cac54c82cd23b867f0d339e6b31d17eeea85461c
Vir <virn.shah@gmail.com>
2022-05-06 12:17:07 -0700

    First Committ

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,13 @@
+# E Calculator
+
+This program calculates the value of 1 / e, which is 0.367879...
+
+First, you enter how many sides you want your dice to have.
+
+The program will then run roll that number of dice as many times as sides it has.
+
+It outputs the chance that you will not roll a specific number, like 1 or 2.
+
+Coincidentally, this value matches very close to 1/e.
+
+The higher the number of sides, the more closer your calculated value will be to 1/e.
---
+++ project.java
@@ -0,0 +1,20 @@
+
+public class project {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.print("Enter how many sides the dice will have: ");
+            double numSide = TextIO.getlnDouble();
+
+            System.out.println("Rolling " + (int)(numSide) + "-sided die " + (int)(numSide) + " times.");
+
+            System.out.println();
+            System.out.println("Chance of not rolling any given number: " + Math.pow((numSide-1.0)/numSide,numSide));
+            System.out.printf("Value of E: %39.9f", 1.0/Math.E);
+            System.out.println();
+
+        }
+        /*
+        */
+    }
+}
