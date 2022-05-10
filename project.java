5824e05ec1d17c69cf1aceb04fa0f24bfd023789
Vir <virn.shah@gmail.com>
2022-05-10 11:05:43 -0700

    Clarifies instructions

--- project.java
+++ project.java
@@ -10,7 +10,7 @@ public class project {
 
             System.out.println();
             System.out.println("Chance of not rolling any given number: " + Math.pow((numSide-1.0)/numSide,numSide));
-            System.out.printf("Value of E: %39.9f", 1.0/Math.E);
+            System.out.printf("Value of 1/E: %37.9f", 1.0/Math.E);
             System.out.println();
 
         }
