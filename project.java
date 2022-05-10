ca158aa1df74b5c0c3ffb71c1353bf18e68ce098
amandaguo1 <105313914+amandaguo1@users.noreply.github.com>
2022-05-10 11:29:17 -0700

    Update project.java

--- project.java
+++ project.java
@@ -7,10 +7,9 @@ public class project {
             double numSide = TextIO.getlnDouble();
 
             System.out.println("Rolling " + (int)(numSide) + "-sided die " + (int)(numSide) + " times.");
-
-            System.out.println();
             System.out.println("Chance of not rolling any given number: " + Math.pow((numSide-1.0)/numSide,numSide));
             System.out.printf("Value of 1/E: %37.9f", 1.0/Math.E);
+            System.out.println();
             System.out.println();
 
         }
